cbde0e8f7757feb597921aadfa9436913ce8e185
Sami-Shafi <freelancersami123@gmail.com>
2023-01-06 03:00:41 +0600

    refactored workspace

---
+++ contracts/lottery.sol
@@ -0,0 +1,11 @@
+// Tasks
+// Enter the lottery by paying
+// pick winner randomly
+// restart lottery every X minute
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract raffle {
+    constructor() {}
+}
